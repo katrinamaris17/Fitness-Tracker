76d863082bc58723e146c4c4e09281284ba155b4
Katrina Arando <katrinamaris17@gmail.com>
2020-12-18 07:19:27 -0800

    started repo

---
+++ server.js
@@ -0,0 +1,43 @@
+require("dotenv").config()
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+
+// Serve up static assets
+// if (process.env.NODE_ENV === "production") 
+// {
+//   app.use(express.static("client/build"));
+// }
+
+// // Add routes, both API and view
+// app.use(routes);
+
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/workout", 
+  {
+    useCreateIndex: true,
+    useNewUrlParser: true
+  }
+);
+
+// routes
+app.use(require("./routes/api.js"));
+app.use(require("./routes/view.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
